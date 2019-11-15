b6fb71952ebe77043adf8d4d437ad69a83795cca
Nhien Nguyen <nhiennt@javisoft.vn>
2019-11-15 23:31:52 +0700

    add Base64UploadAdapter

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -54,7 +56,8 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Base64UploadAdapter
 ];
 
 // Editor configuration.
